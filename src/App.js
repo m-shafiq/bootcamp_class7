57974e8f750b1d55fd45cd2b569adc42ea500bf5
MuhammadShafiq <shafiqsaqisc@gmail.com>
2020-06-27 23:19:24 +0500

    useeffect practice

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+
 import './App.css';
 
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setdata] = useState([{}]);
+  useEffect(() => {
+    async function getRepo() {
+      const response = await fetch('https://api.github.com/users/m-shafiq/repos');
+      const data = await response.json();
+      console.log(data);
+      setdata(data);
+    }
+    getRepo()
+
+    //fetch('https://jsonplaceholder.typicode.com/posts/1')
+    // .then(response => response.json())
+    // .then(json => {
+    //   console.log(json);
+    //   setdata(json);
+
+  
+  
+  
+}, [])
+return (
+  <div className="App">
+
+    <h1> Hello from Shafiq, Here are my Github Repositories</h1>
+    <hr />
+    <ul>
+      {data.map(
+        (repoObj)=>{
+          return <li>
+            {repoObj.name}
+          </li>
+        }
+      )}
+    </ul>
+
+
+
+  </div>
+
+);
 }
 
 export default App;
